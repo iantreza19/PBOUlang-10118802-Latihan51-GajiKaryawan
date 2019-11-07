246dd7c58442c1cc490e9f5b6b25dbb94ab98778
iantreza19 <55836534+iantreza19@users.noreply.github.com>
2019-11-07 22:59:28 +0700

    Add files via upload

---
+++ PBOUlang10118802Latihan51/src/Manager.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IantReza
+ */
+public class Manager {
+    private int kehadiran;    
+    private float tunjanganGolongan;
+    private float tunjanganJabatan;
+    private float tunjanganKehadiran;
+
+    public int getKehadiran() {
+        return kehadiran;
+    }
+
+    public void setKehadiran(int kehadiran) {
+        this.kehadiran = kehadiran;
+    }
+    
+    public float tunjanganKehadiran(int hadir) {
+       return kehadiran * 10000;
+        
+    }
+    
+    public float tunjanganJabatan(String jabatan) {
+       
+        if(null == jabatan)tunjanganJabatan = 0;
+        else switch (jabatan) {
+            case "Manager":
+                tunjanganJabatan = 2000000;
+                break;
+            case "Kabag":
+                tunjanganJabatan = 1000000;
+                break;
+            default:
+                tunjanganJabatan = 0;
+                break;
+        }
+        
+        return tunjanganJabatan;
+        
+    }
+    
+    public float tunjanganGolongan(int golongan) {
+       
+        switch (golongan) {
+            case 1:
+                tunjanganGolongan = 500000;
+                break;
+            case 2:
+                tunjanganGolongan = 1000000;
+                break;
+            case 3:
+                tunjanganGolongan = 1500000;
+                break;
+            default: 
+                tunjanganGolongan = 0;
+                break;
+        }
+       
+           return tunjanganGolongan;
+        
+    }
+    
+   public float gajiTotal() {
+   return tunjanganJabatan + tunjanganGolongan + tunjanganKehadiran(kehadiran);
+        
+    }
+}
